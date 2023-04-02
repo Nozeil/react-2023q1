22a91433822a3851f13e64f81a59b3f500ca7097
Nozeil <nozeilwrk@gmail.com>
2023-04-03 01:28:32 +0300

    feat: add styles for form cards

--- app/src/components/Form/FormCardList/FormCardList.tsx
+++ app/src/components/Form/FormCardList/FormCardList.tsx
@@ -1,9 +1,10 @@
 import FormCard from '../FormCard/FormCard';
 import { FormCardListPropsI } from './FormCardList.interfaces';
 import { TestIds } from './../Form.enums';
+import cl from './FormCardList.styles.module.css';
 
 const FormCardList = ({ cards }: FormCardListPropsI) => (
-  <div data-testid={TestIds.formCardList}>
+  <div data-testid={TestIds.formCardList} className={cl.list}>
     {cards.map((card) => (
       <FormCard data={card.data} key={card.id} />
     ))}
